79738297f1e850868fe0f60e5888739105220b16
ShiryevIgorVl <ishirayev@gmail.com>
2020-08-30 18:17:36 +0300

    1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,13 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller {
+    @FXML
+    public Button clTArea;
     @FXML
     private TextArea tArea;
     @FXML
@@ -16,5 +19,10 @@ public class Controller {
         String text = tField.getText();
         tArea.appendText(text + "\n");
         tField.clear();
+        tField.requestFocus();
+    }
+
+    public void clearTextArea(ActionEvent actionEvent) {
+        tArea.clear();
     }
 }
